f00cb3668808489b85efc83b5a5732b1fd283ce0
Alessandro Speranza <asperanz109@outlook.com>
2022-11-19 13:31:21 +0200

    Initial commit

---
+++ Development/Shiny/global.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+# Get data from github
+dd_concerts <- as_tibble(read.csv(file="https://raw.githubusercontent.com/asperanz/Dead-Dogs-Analytics-ShinyApp/master/data/Dead%20Dogs%20Live%20Concerts.csv", header=TRUE, sep=",", stringsAsFactors=FALSE))
+
+# Get data locally from file system 
+# dd_concerts <- as_tibble(read.csv("./data/Dead Dogs Live Concerts.csv", header=TRUE, sep=",", stringsAsFactors=FALSE))
