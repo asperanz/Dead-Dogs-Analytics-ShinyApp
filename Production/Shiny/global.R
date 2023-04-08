4e6dbad9c9899fe44c5ad084ea78fd28335b3588
Alessandro Speranza <asperanz109@outlook.com>
2023-04-08 22:45:09 +0300

    Fixed the path file location: from Development to Production

--- Production/Shiny/global.R
+++ Production/Shiny/global.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # Get data from github
-dd_concerts <- as_tibble(read.csv(file="https://raw.githubusercontent.com/asperanz/Dead-Dogs-Analytics-ShinyApp/main/Development/Shiny/data/Dead%20Dogs%20Live%20Concerts.csv", header=TRUE, sep=",", stringsAsFactors=FALSE))
+dd_concerts <- as_tibble(read.csv(file="https://raw.githubusercontent.com/asperanz/Dead-Dogs-Analytics-ShinyApp/main/Production/Shiny/data/Dead%20Dogs%20Live%20Concerts.csv", header=TRUE, sep=",", stringsAsFactors=FALSE))
 
 # Get data locally from file system 
 # dd_concerts <- as_tibble(read.csv("./data/Dead Dogs Live Concerts.csv", header=TRUE, sep=",", stringsAsFactors=FALSE))
